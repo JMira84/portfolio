246bd2e6aa839a6f43f8980a56aedb1e5d4d926e
João Mira <joao.mira01@gmail.com>
2020-08-20 15:03:47 +0100

    created header

---
+++ views/layouts/header.php
@@ -0,0 +1,34 @@
+<header class="py-3 position-relative">
+    <div class="container">
+        <div class="logo-and-nav d-flex justify-content-between align-items-center">
+            <div class="header-logo">
+                <img src="assets/images/logo2.png">
+            </div>
+
+            <div class="hamburger cursor-pointer display-none">
+                <div class="top"></div>
+                <div class="middle"></div>
+                <div class="bottom"></div>
+            </div>
+
+            <nav class="nav-menu font-500">
+                <ul class="list-unstyled d-flex m-0">
+                    <li class="pr-md-5">
+                        <a class="pb-lg-1 white-text" href="#">home</a>
+                    </li>
+                    <li class="pr-md-5">
+                        <a class="pb-lg-1 white-text" href="#">sobre mim</a>
+                    </li>
+                    <li class="pr-md-5">
+                        <a class="pb-lg-1 white-text" href="#">portfolio</a>
+                    </li>
+                    <li>
+                        <a class="pb-lg-1 white-text" href="#">contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <!--end logo-and-nav-->
+    </div>
+    <!--end container-->
+</header>
