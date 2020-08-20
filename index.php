53e9eaf06d0793b9448034e839667be83c8ca4b3
João Mira <joao.mira01@gmail.com>
2020-08-20 12:28:36 +0100

    Initial commit

--- index.php
+++ index.php
@@ -5,10 +5,4 @@ define("HOME_PATH", dirname($_SERVER["SCRIPT_NAME"]));
 
 $controller = "home";
 
-$controllers = ["home", "about", "portfolio", "contact"];
-
-if(isset($url_parts[1]) && in_array($url_parts[1], $controllers)) {
-    $controller = $url_parts[1];
-}
-
 require("controllers/" . $controller . ".php");
---
+++ views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>João Mira - Portfólio</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="/resources/css/styles.css">
+        <script src="/resources/js/app.js" defer></script>
+    </head>
+    <body>
+    </body>
+</html>
