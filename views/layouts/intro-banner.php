a8ff539842ffbced9c386374e43d687819982221
João Mira <joao.mira01@gmail.com>
2020-08-20 22:14:53 +0100

    created animated banner

---
+++ views/layouts/intro-banner.php
@@ -0,0 +1,18 @@
+<div class="intro white-text d-flex flex-column justify-content-center align-items-center">
+    <h2 class="h4 mb-4 mb-md-3">Bem-vindo</h2>
+    <div class="typewriter d-flex mb-sm-3">
+        <h3 class="pre-title pr-2 pr-md-3">Sou</h3>
+        <h3 class="typewriter-title">o João.</h3>
+    </div>
+
+    <div class="download-cv mt-5">
+        <a class="cv-download rounded-pill p-3 white-text" href="https://drive.google.com/file/d/1Go-8mItg4q1X2fEnvJWoODxSqEdAHAIU/view?usp=sharing" target="_blank">Download CV</a>
+    </div>
+
+    <div class="intro-icon position-absolute mb-3">
+        <a class="white-text" href="#about">
+            <i class="fas fa-chevron-down fa-2x"></i>
+        </a>
+    </div>
+</div>
+<!--end intro-->
