b7ba2e3a3ecb411aa35547631d20fd356757c239
João Mira <joao.mira01@gmail.com>
2020-08-24 16:14:46 +0100

    created about layout

---
+++ views/layouts/about.php
@@ -0,0 +1,218 @@
+<div class="about-wrapper px-3 px-md-0 py-5 font-raleway" id="about">
+    <div class="container">
+        <div class="about-title d-flex justify-content-center mb-5">
+            <h2 class="h1 font-800">
+                Sobre <span class="font-300">mim</span>
+            </h2>
+        </div>
+
+        <div class="info-wrapper row">
+            <div class="info-pic col-lg-3 rounded mb-5 mb-lg-0 p-0">
+                <img class="mw-100 rounded" src="/assets/images/IMG_2817.jpg" alt="A picture of João Mira">
+            </div>
+
+            <div class="info-text col-lg-9 pl-md-4">
+                <p>Olá! Eu sou o João e nasci em Lisboa a 28 de Março de 1984.</p>
+                <p>Em 2008 licenciei-me em <span class="font-600">Comunicação Aplicada: Marketing, Publicidade e Relações Públicas</span>, na Universidade Lusófona de Humanidades e Tecnologias. Em 2010 concluí os meus estudos no IADE - Escola Superior de Marketing e Publicidade, com uma <span class="font-600">pós-graduação em publicidade.</span></p>
+                <p>Trabalhei alguns anos como copywriter publicitário, até que resolvi procurar um novo desafio. Decidi então começar a estudar programação e inscrevi-me no curso de <span class="font-600">Full-Stack Web Development</span> da FLAG, o qual terminei em 2020.</p>
+                <p>As minhas competências são variadas, abrangendo tanto o <span class="font-600">front-end</span> (HTML, CSS e Javascript), como o <span class="font-600">back-end</span> (PHP e MySQL).</p>
+            </div>
+            <!--end info-text-->
+        </div>
+        <!--end info-wrapper--->
+
+        <div class="skills-wrapper mt-5 pb-4">
+            <h3 class="h4 mb-4 text-center font-300">
+                As minhas <span class="font-800">competências</span>
+            </h3>
+            <div class="skills-graph row pt-4">
+                <div class="skills col-md-6 mb-4 mb-sm-0">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="h6 font-600">HTML</h3>
+                        <span>70%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="html color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">CSS</h3>
+                        <span>65%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="css color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">Javascript</h3>
+                        <span>60%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="js color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">Bootstrap</h3>
+                        <span>60%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="js color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">jQuery</h3>
+                        <span>40%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="jquery color-bar"></div>
+                    </div>
+                </div>
+                <!--end skills-->
+
+                <div class="skills col-md-6">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="h6 font-600">Angular</h3>
+                        <span>30%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="angular color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">PHP</h3>
+                        <span>65%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="php color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">MySQL</h3>
+                        <span>60%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="mysql color-bar"></div>
+                    </div>
+
+                    <div class="d-flex justify-content-between pt-4">
+                        <h3 class="h6 font-600">Laravel</h3>
+                        <span>30%</span>
+                    </div>
+                    <div class="skill-bar w-100">
+                        <div class="laravel color-bar"></div>
+                    </div>
+                </div>
+                <!--end skill-->
+            </div>
+            <!--end skills-graph-->
+        </div>
+        <!--end skills-wrapper-->
+
+        <div class="experience-wrapper mt-5">
+            <h3 class="h4 mb-4 text-center font-300">
+                O meu <span class="font-800">percurso</span>
+            </h3>
+
+            <div class="studies-and-work row pt-4">
+                <div class="studies exp col-md-6">
+                    <ul class="list-unstyled">
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-graduation-cap"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2019 - 2020</span>
+                            <h5 class="text-uppercase">
+                                Curso Profissional
+                                <span class="place position-relative font-raleway font-600">FLAG</span>
+                            </h5>
+                            <p class="font-raleway">
+                                Curso de Full-Stack Web Development
+                            </p>
+                        </li>
+
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-graduation-cap"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2009 - 2010</span>
+                            <h5 class="text-uppercase">
+                                Pós-Graduação
+                                <span class="place position-relative font-raleway font-600">IADE</span>
+                            </h5>
+                            <p class="font-raleway">
+                                Pós-Graduação em Publicidade
+                            </p>
+                        </li>
+
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-graduation-cap"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2005 - 2008</span>
+                            <h5 class="text-uppercase">
+                                Licenciatura
+                                <span class="place position-relative font-raleway font-600">Universidade Lusófona</span>
+                            </h5>
+                            <p class="font-raleway">
+                                Licenciatura em Comunicação Aplicada: Marketing, Publicidade e Relações Públicas
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <!--end studies-->
+
+                <div class="work exp col-md-6">
+                    <ul class="list-unstyled">
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-briefcase"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2017 - 2019</span>
+                            <h5 class="text-uppercase">
+                                Turismo de Habitação
+                                <span class="place position-relative font-raleway font-600">Negócio em nome próprio</span>
+                            </h5>
+                            <p class="font-raleway">
+                                Após sair da DDB Portugal, trabalhei durante algum tempo em turismo de habitação.
+                            </p>
+                        </li>
+
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-briefcase"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2014 - 2016</span>
+                            <h5 class="text-uppercase">
+                                Copywriter
+                                <span class="place position-relative font-raleway font-600">DDB Portugal</span>
+                            </h5>
+                            <p class="font-raleway">
+                                Em 2014 dei continuidade ao meu percurso na DDB Portugal.
+                            </p>
+                        </li>
+
+                        <li class="position-relative">
+                            <div class="exp-icon position-absolute rounded-circle white-text">
+                                <i class="fas fa-briefcase"></i>
+                            </div>
+                            <span class="time text-uppercase white-text font-raleway font-600">2010 - 2014</span>
+                            <h5 class="text-uppercase">
+                                Copywriter
+                                <span class="place position-relative font-raleway font-600">Havas Portugal</span>
+                            </h5>
+                            <p class="font-raleway">
+                                O meu percurso profissional começou na Havas Portugal, em 2010, como copywriter publicitário.
+                            </p>
+                        </li>
+
+                    </ul>
+                </div>
+                <!--end work-->
+            </div>
+            <!--end studies-and-work-->
+        </div>
+        <!--experience-wrapper-->
+    </div>
+    <!--end container-->
+</div>
+<!--end about-wrapper-->
